3ab752bd7581076d827fd01abc099313191d9eb9
vaidehi <vaidehi117v@gmail.com>
2024-09-10 12:11:42 -0400

    sql table

---
+++ admins.sql
@@ -0,0 +1,24 @@
+-- Active: 1725981826967@@sql1.njit.edu@3306
+SHOW DATABASES;
+
+CREATE TABLE admins (
+ adminID      INT(11)      NOT NULL   AUTO_INCREMENT,
+ emailAddress VARCHAR(255) NOT NULL   UNIQUE,
+ password     CHAR(64)     NOT NULL,
+ firstName    VARCHAR(60)  NOT NULL,
+ lastName     VARCHAR(60)  NOT NULL,
+  PRIMARY KEY (adminID)
+);
+
+SHOW TABLES;
+
+SHOW CREATE TABLE admins;
+
+DESCRIBE admins;
+
+INSERT INTO admins
+(emailAddress, password, firstName, lastName)
+VALUES
+('taylor@guitarshop.com',
+SHA2('myL0ngP@ssword', 256), 'Taylor', 'Swift');
+
